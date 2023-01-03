c81b131b70d6dd8c6c7207b13a60a36aa1e21f0a
Annastacia Mumbua Kioko <annastacia.kioko@student.moringaschool.com>
2023-01-03 11:27:55 +0300

    completed

--- src/features/users/Users.js
+++ src/features/users/Users.js
@@ -1,13 +1,23 @@
 import React from "react";
 // add any needed imports here
+import { useSelector } from 'react-redux'
 
 function Users() {
+  // add code here
+  const users = useSelector(state => state.users)
   return (
     <div>
       <ul>
         Users!
         {/* Write code here that displays the usernames of all users in the Redux store */}
+        {
+          users.map((user, index) => <li key={index}>
+            username: {user.username}
+            hometown: {user.hometown}
+            </li>)
+        }
         {/* In addition, display the total number of users curently in the store */}
+        <p>Total Users: {users.length}</p>
       </ul>
     </div>
   );
